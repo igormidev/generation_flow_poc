a99bc9fbfeeeabc3c5971ff99748141128245ac8
Igor <igor_midev@outlook.com>
2024-01-31 05:52:51 -0300

    Site upload

--- android/app/src/main/kotlin/com/generation/flow/generation_flow_poc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.generation.flow.generation_flow_poc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/models/episode_time_enum.dart
+++
@@ -1,5 +0,0 @@
-enum EEpisodeTime {
-  five,
-  ten,
-  twenty;
-}
